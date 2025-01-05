8357494e34bca43cb2a94136f37419d0a75cf140
Swam Htet Aung <swamhtetaungg@gmail.com>
2025-01-05 09:44:28 +0700

    setup sanctum + api routes

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/sign-in', function (Request $request) {
+    return [
+        'success' => true
+    ];
+});
